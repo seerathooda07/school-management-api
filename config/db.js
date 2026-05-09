7ff66b4fe5e1396fdfd96af42bfb08117b7fee1b
seerathooda07 <seerat4505@gmail.com>
2026-05-09 15:15:35 +0530

    some minor change

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
     logging: false,
   }
