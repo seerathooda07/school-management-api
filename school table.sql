6fc48fe4879918316ca12f9c42c9f20a20cb26cd
seerathooda07 <seerat4505@gmail.com>
2026-05-13 17:19:09 +0530

    Initial commit

--- school table.sql
+++
@@ -1,9 +0,0 @@
-show databases;
-use school_management;
-CREATE TABLE schools (
-id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR(500) NOT NULL,
-    latitude FLOAT NOT NULL,
-    longitude FLOAT NOT NULL
-);
